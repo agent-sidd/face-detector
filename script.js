831397bebcf0a7eb0cfd443f86d3d709410aadfc
Sidddharth singh <60338719+agent-sidd@users.noreply.github.com>
2020-07-23 13:11:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const video=document.getElementById('video');
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-    faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-    faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-    faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+    faceapi.nets.tinyFaceDetector.loadFromUri("models"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("models"),
+    faceapi.nets.faceRecognitionNet.loadFromUri("models"),
+    faceapi.nets.faceExpressionNet.loadFromUri("models"),
 ]).then(startvideo);
 function startvideo(){
     navigator.getUserMedia(
