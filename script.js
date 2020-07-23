ef184c482a9ba962c8a85d7bbdc34b5dad34126e
Sidddharth singh <60338719+agent-sidd@users.noreply.github.com>
2020-07-23 13:16:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const video=document.getElementById('video');
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri("models"),
-    faceapi.nets.faceLandmark68Net.loadFromUri("models"),
-    faceapi.nets.faceRecognitionNet.loadFromUri("models"),
-    faceapi.nets.faceExpressionNet.loadFromUri("models"),
+    faceapi.nets.tinyFaceDetector.loadFromUri("/face-detector/tree/master/models"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("/face-detector/tree/master/models"),
+    faceapi.nets.faceRecognitionNet.loadFromUri("/face-detector/tree/master/models"),
+    faceapi.nets.faceExpressionNet.loadFromUri("/face-detector/tree/master/models"),
 ]).then(startvideo);
 function startvideo(){
     navigator.getUserMedia(
